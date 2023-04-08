94316b1d6650b80ceeebbc8768d48f151f1e6d4d
Rafael Galvao <rafaelgalvaolm.dev@gmail.com>
2023-04-07 22:52:30 -0300

    card 3 recebendo informacoes certas

--- Trabalho Cronometro/script/timer.js
+++ Trabalho Cronometro/script/timer.js
@@ -1,4 +1,5 @@
 //Primeira parte dedicada ao cronometro
+
 const timer = document.querySelector(".timer");
 const btnStartPause = document.getElementById("btnStartPause");
 const btnStop = document.getElementById("btnStop");
@@ -17,6 +18,7 @@ function updateTimer() {
   const seconds = Math.floor((elapsedTime / 1000) % 60).toString().padStart(2, "0");
   timer.textContent = `${hours}:${minutes}:${seconds}`;
 }
+
 btnStartPause.addEventListener("click", function(e) {
   e.preventDefault();
   if (isPaused) {
@@ -37,11 +39,41 @@ btnStop.addEventListener("click", function(e) {
   timer.textContent = "00:00:00";
 });
 
-btnReset.addEventListener("click", function(e) {
-  e.preventDefault();
+
+//Segunda parte dedicada a inserir os dados corretos dentro do card 3
+
+btnReset.addEventListener("click", function(a)
+{
+  a.preventDefault();
+  clearInterval(intervalId);
+  isPaused = true;
+  addColuna(elapsedTime);  
   elapsedTime = 0;
-  timer.textContent = "00:00:00";
+  timer.textContent = intervalId;
 });
+
+function addColuna(elapsedTime)
+{
+
+  const usuario = document.getElementById("Usuario").value;
+  const atividade = document.getElementById("Atividade").value;
+  const tipoAtividade = document.getElementById("TipoAtividade").value;
+  const tempo = document.getElementById("Timer").innerHTML;
+
+  var table = document.getElementById("Tabela");
+  var row = table.insertRow(-1);
+
+  var usuarioCell = row.insertCell(0);
+  usuarioCell.innerHTML = usuario;
+  var atividadeCell = row.insertCell(1);
+  atividadeCell.innerHTML = atividade;
+  var tipoAtividadeCell = row.insertCell(2);
+  tipoAtividadeCell.innerHTML = tipoAtividade;
+  var tempoCell = row.insertCell(3);
+  tempoCell.innerHTML = tempo;
+
+}
+
 
 
 
