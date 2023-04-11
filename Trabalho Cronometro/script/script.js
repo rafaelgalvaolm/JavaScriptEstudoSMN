da83bc3b001ee399d4de8566459d2d7ce17fae6e
Rafael Galvao <rafaelgalvaolm.dev@gmail.com>
2023-04-11 00:40:21 -0300

    criado novo js, funcionalidade de soma criada

--- Trabalho Cronometro/script/script.js
+++ Trabalho Cronometro/script/script.js
@@ -1,86 +1,127 @@
-//Primeira parte dedicada ao cronometro
+//Primeira parte criada em razao da logica do timer e play e pause
+const usuario = document.getElementById('Usuario');
+const atividade = document.getElementById('Atividade');
+const tipoatividade = document.getElementById('TipoAtividade');
+const timer = document.getElementById('Timer');
+const btnPlayPause = document.getElementById('btnStartPause');
+const btnStop = document.getElementById('btnStop');
+const btnEditar = document.getElementById('btnEditar');
+const btnDeletar = document.getElementById('btnDeletar');
+let passarTempo;
 
-const timer = document.querySelector(".timer");
-const btnStartPause = document.getElementById("btnStartPause");
-const btnStop = document.getElementById("btnStop");
-
-let intervalId;
-let startTime;
-let elapsedTime = 0;
-let isPaused = true;
-
-function updateTimer() 
+function incrementarTempo()
 {
-  const now = Date.now();
-  elapsedTime = now - startTime;
-  hora(elapsedTime);
+  let timerString = timer.innerHTML;
+  let timerArray = timerString.split(':');
+  let horas = parseInt(timerArray[0]);
+  let minutos = parseInt(timerArray[1]);
+  let segundos = parseInt(timerArray[2]);
+
+  segundos++;
+
+  if(segundos == 60)
+  {
+    segundos = 0;
+    minutos++;
+  }
+
+  if(minutos == 60)
+  {
+    minutos = 0;
+    horas++;
+  }
+
+  timer.innerHTML = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`
+
 }
 
-function hora(elapsedTime)
+btnPlayPause.addEventListener('click', function(playPause)
 {
-  const hours = Math.floor(elapsedTime / (1000 * 60 * 60)).toString().padStart(2, "0");
-  const minutes = Math.floor((elapsedTime / (1000 * 60)) % 60).toString().padStart(2, "0");
-  const seconds = Math.floor((elapsedTime / 1000) % 60).toString().padStart(2, "0");
-  timer.textContent = `${hours}:${minutes}:${seconds}`;
-}
+  playPause.preventDefault();
 
-btnStartPause.addEventListener("click", function(e) {
-  e.preventDefault();
-  if (isPaused) {
-    startTime = Date.now() - elapsedTime;
-    intervalId = setInterval(updateTimer, 1000);
-    isPaused = false;
+  if(passarTempo)
+  {
+    clearInterval(passarTempo);
+    passarTempo = null;
   } else {
-    clearInterval(intervalId);
-    isPaused = true;
+    passarTempo = setInterval(incrementarTempo, 1000);
   }
 });
 
-//Segunda parte dedicada a inserir os dados corretos dentro do card 3
-
-btnStop.addEventListener("click", function(a)
+// Segunda parte criada em razao da necessidade de adicionar uma coluna na tabela
+btnStop.addEventListener('click', function(stop)
 {
-  a.preventDefault();
-  clearInterval(intervalId);
-  isPaused = true;
-  hora(elapsedTime);
+  stop.preventDefault();
+  clearInterval(passarTempo);
+  passarTempo = null;
   addColuna();
-  timer.textContent = "00:00:00";
+  timer.innerHTML = "00:00:00";
 });
+
 
 function addColuna()
 {
-  const usuario = document.getElementById("Usuario").value;
-  const atividade = document.getElementById("Atividade").value;
-  const tipoAtividade = document.getElementById("TipoAtividade").value;
-  const tempo = timer.textContent;
-  const elapsedTime = parseInt(tempo)
+  let valorUsuario = usuario.value;
+  let valorAtividade = atividade.value;
+  let valorTipoAtividade = tipoatividade.value;
+  let valorTimer = timer.innerHTML;
+  let corpoTabela = document.getElementById('TabelaBody');
+  let row = corpoTabela.insertRow(0);
 
-  var table = document.getElementById("TabelaBody");
-
-  for(var i = 0; i < table.rows.length; i++)
+  for(let i = 0; i < corpoTabela.rows.length; i++)
   {
-    var row = table.rows[i];
-    if(row.id == usuario + '_' + atividade + '_' + tipoAtividade)
+    let row = corpoTabela.rows[i];
+    let cell1 = row.cells[0];
+    let cell2 = row.cells[1];
+    let cell3 = row.cells[2];
+    let cell4 = row.cells[3];
+
+    if
+    (
+      cell1 && cell1.innerHTML == valorUsuario &&
+      cell2 && cell2.innerHTML == valorAtividade &&
+      cell3 && cell3.innerHTML == valorTipoAtividade
+    )
     {
-      var tempoCell = row.cells[3];
-      var tempoAntigo = tempoCell.innerText;
-      var tempoNovo = Date.now() + parseInt(tempoAntigo) + parseInt(elapsedTime);
-      tempoCell.innerHTML = hora(tempoNovo);
+      let timerArray = cell4.innerHTML.split(':');
+      let timerArray2 = valorTimer.split(':');
+      let horasVelho = parseInt(timerArray[0]);
+      let minutosVelho = parseInt(timerArray[1]);
+      let segundosVelho = parseInt(timerArray[2]);
+      let horasNovo = parseInt(timerArray2[0]);
+      let minutosNovo = parseInt(timerArray2[1]);
+      let segundosNovo = parseInt(timerArray2[2]);
+
+      segundosVelho += segundosNovo;
+      minutosVelho += minutosNovo;
+      horasVelho += horasNovo;
+
+      if(segundosVelho >= 60)
+      {
+        segundosVelho -= 60;
+        minutosVelho++;
+      }
+
+      if(minutosVelho >= 60)
+      {
+        minutosVelho -= 60;
+        horasVelho++;
+      }
+
+      cell4.innerHTML = `${horasVelho.toString().padStart(2, '0')}:${minutosVelho.toString().padStart(2, '0')}:${segundosVelho.toString().padStart(2, '0')}`;
+      
       return;
     }
   }
+  row = corpoTabela.insertRow(0);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  let cell4 = row.insertCell(3);
+  cell1.innerHTML = valorUsuario;
+  cell2.innerHTML = valorAtividade;
+  cell3.innerHTML = valorTipoAtividade;
+  cell4.innerHTML = valorTimer;
+};
 
-  var row = table.insertRow(-1);
-  row.id = usuario + '_' + atividade + '_' + tipoAtividade;
-  var usuarioCell = row.insertCell(0);
-  usuarioCell.innerHTML = usuario;
-  var atividadeCell = row.insertCell(1);
-  atividadeCell.innerHTML = atividade;
-  var tipoAtividadeCell = row.insertCell(2);
-  tipoAtividadeCell.innerHTML = tipoAtividade;
-  var tempoCell = row.insertCell(3);
-  tempoCell.innerHTML = hora(elapsedTime);
-}
-
-
+//terceira parte criada em razao da necessidade de se poder editar as tabelas ja criadas ou deleta-las
